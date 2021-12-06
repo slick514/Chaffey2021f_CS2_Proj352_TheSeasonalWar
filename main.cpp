59356cd3c243a446753f368d6d22da65f3a8bcf5
Justin Gries <justin.gries@gmail.com>
2021-12-06 14:29:43 -0800

    Documentation

--- main.cpp
+++ main.cpp
@@ -9,37 +9,34 @@ static const char *const OUTPUT_FILEPATH = "./output.txt";
 using namespace std;
 
 /**
+ * CHAFFEY COLLEGE FALL 2021; COMPSCI-2; ICPC PROJECT; GROUP 12
+ * Group Members: Justin Gries & Christian Flores
+ * Code link (Repl.it): https://replit.com/@JustinGries/Chaffey2021fCS2Proj352TheSeasonalWar#main.cpp
  * P352: "The seasonal war"
  * See attached description: p352.pdf
- *
- * Algorithm:
- *  Read in first line
-    Parse data_structure size, "n"
-    Create 2D, square data_structure consisting of vectors of "n x n" size, from the next "n" lines of file input
-    Call an EagleSearcher to search the data_structure
-    Print result of data_structure search
-    If EOF is not reached, read in the next "section" and handle in similar fashion
  */
 int main() {
+    // Variable Instantiation
     ifstream input_fstream; // input stream to bring info from input file
     ofstream output_fstream; // output stream to write info to the output file
-    int grid_count = 0; // keeps track of the number of grids that have been parsed/analyzed
+    int grid_count = 0; // keeps track of the number of grids/arrays that have been parsed/analyzed
     int size; // will hold the size (side-length) of each square-data_structure
     EagleSearcher searcher; // will do the lifting for data_structure analysis
     string input_line; // will hold each line as it is read in from the input stream
     struct EagleSearcher::eagle_grid grid; // will hold each parsed data_structure
     int found_eagles; // will hold the number of found eagles for each iteration.
+    //---- End Variable Instantiation ----
 
     open_filestreams(input_fstream, output_fstream); // opens both file-streams
 
     while(getline(input_fstream, input_line)){ // while there are more lines of input...
-        grid_count++; // iterate the data_structure count.
+        grid_count++; // iterate the grid/array-count.
         size = (int)stoi(input_line); // read in the size of the subsequent data_structure. (Max characters: 25)
 
-        // create a data_structure from the next "size" lines of input
+        // create a grid/array from the next "size" lines of input
         grid = EagleSearcher::generate_grid(size, input_fstream);
 
-        found_eagles = searcher.search(grid); // search the data_structure for "eagles"
+        found_eagles = searcher.search(grid); // search the grid for "eagles"
 
         // write the output to the output file
         EagleSearcher::write_output(grid_count, output_fstream, found_eagles);
